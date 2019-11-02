dc3fe8ec249dbf439f2feb6fca1514e32301e44d
ArbaazKhan1 <bazzy_94@yahoo.com>
2019-11-02 13:35:24 -0500

    Added power management methods

--- src/main/java/edu/depaul/cdm/PowerManagement.java
+++ src/main/java/edu/depaul/cdm/PowerManagement.java
@@ -1,5 +1,24 @@
 package edu.depaul.cdm;
 
 public class PowerManagement {
+    private int batteryPower = 250;
+    private int powerThreshold = (int) (250*.2);    //power threshold is at 20%
+
+    public void setBatteryPower(int batteryPower) {
+        this.batteryPower = batteryPower;
+    }
+
+    public int getBatteryPower() {
+        return batteryPower;
+    }
+
+    public Boolean lowPowerAlert(){ //Will return true if power is less then 20%
+        if(batteryPower<powerThreshold){return true;}
+        return false;
+    }
+    public void vacuum(){
+
+    }
+
 
 }
