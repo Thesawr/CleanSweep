94b981a8e32cea84f63faf602b542566e66722f4
hamood <h3rehman@gmail.com>
2019-11-05 00:20:41 -0600

    Made a Copy of the 2d Array that was getting overridden due to the ShortestPath Singleton.

--- src/main/java/edu/depaul/cdm/Main.java
+++ src/main/java/edu/depaul/cdm/Main.java
@@ -7,6 +7,15 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Main {
+
+	//Copy of Original Floor Plan
+	static void make2DCopy (int[][] array, int[][] arrayCopy) {
+		for (int i=0; i<array.length; i++){
+			for (int j=0; j<array[0].length; j++){
+				arrayCopy[i][j] = array[i][j];
+			}
+		}
+	}
 	
 	//Print 2D Array 
 	static void print2DArray (int[][] array) {
@@ -23,22 +32,26 @@ public class Main {
 		
 		Object obj = new JSONParser().parse(new FileReader("src/main/resources/MultiRoomNoObjects.json"));
 		int[][] twoDArray = ParseFloorPlan.getInstance().parse_func(obj);
-
+		int[][] twoDArrayCopy = new int[twoDArray.length][twoDArray[0].length];   
+		make2DCopy(twoDArray, twoDArrayCopy); //Avoiding global state in ShortestPath Singleton
+		
 		Locator locator = Locator.getInstance();
 		locator.setStarter(twoDArray);
 		int x = locator.getX();
 		int y = locator.getY();
-
+		
 		PowerManagement powerManagement = PowerManagement.getInstance();
 		powerManagement.set_floor(twoDArray);
-
+		
+		print2DArray(twoDArray);
+		
 		ShortestPath shortestPath = ShortestPath.getInstance();
-		shortestPath.setCordsnArray(x, y, twoDArray);
+		shortestPath.setCordsnArray(x, y, twoDArrayCopy);
 		shortestPath.allPointsShortestDistance();  //Calculates the shortest distance 
 		int[][]shortestDist = shortestPath.getShortestPath(); //will get the 2D Array for Shortest Distance to Charger
 		
 //		print2DArray(shortestDist); //Uncomment to Print all Traversable points shortest distance to charger
-
+		
 		MoveRobot moveRobo = new MoveRobot(twoDArray, x, y, shortestPath.getTraverableUnits(), shortestDist);
 		moveRobo.move();
 	}
